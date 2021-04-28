cbedc3c7bd0001dc04d2fedcbce5d61287a5ccbb
Maja Reichert <stu126115@mail.uni-kiel.de>
2021-04-28 14:14:48 +0200

    Add skeletons for workout and exercise components

---
+++ frontend/src/js/components/exercise/TimedExercise.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TimedExercise = ({ className }) => {
+  return <div className={className}>TimedExercise</div>;
+};
+
+export default styled(TimedExercise);
---
+++ frontend/src/js/components/workout/Workout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Workout = ({ className }) => {
+  return <div className={className}>Workout</div>;
+};
+
+export default styled(Workout);
