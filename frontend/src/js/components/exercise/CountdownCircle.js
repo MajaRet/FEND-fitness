47f57c8b73497fbc5253da612a72d685e25cfe95
Maja Reichert <stu126115@mail.uni-kiel.de>
2021-05-04 11:19:34 +0200

    Change size of countdown timer

--- frontend/src/js/components/exercise/CountdownCircle.js
+++ frontend/src/js/components/exercise/CountdownCircle.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+import CircularProgressBar from './../CircularProgressBar';
+
 const CountdownCircle = React.forwardRef(
   ({ className, seconds, secondsLeft }, ref) => {
     const [started, setStarted] = useState(false);
@@ -11,65 +13,26 @@ const CountdownCircle = React.forwardRef(
       ref.current.style = 'transition: stroke-dashoffset 1s ease-in-out;';
     }, [ref]);
 
-    const radius = 80;
-    const circumference = 2 * Math.PI * radius;
+    // TODO set it relative to window width or put media queries.
+    const radius = 175;
+
     const progressOffset = started
-      ? (1 - Math.max(secondsLeft - 1, 0) / seconds) * circumference
+      ? (1 - Math.max(secondsLeft - 1, 0) / seconds) * 2 * Math.PI * radius
       : 0;
+
     return (
-      <div className={className}>
-        <svg width="200" height="200">
-          <circle
-            className="circle background"
-            r={radius}
-            strokeWidth="22"
-            cx="100"
-            cy="100"
-          ></circle>
-          <circle
-            className="circle progress"
-            r={radius}
-            ref={ref}
-            strokeWidth="22"
-            cx="100"
-            cy="100"
-            strokeDasharray={circumference}
-            strokeDashoffset={progressOffset}
-          ></circle>
-          <text
-            className="seconds"
-            x="50%"
-            y="50%"
-            textAnchor="middle"
-            dy="10px"
-          >
-            {secondsLeft}s
-          </text>
-        </svg>
-      </div>
+      <CircularProgressBar
+        className={className}
+        ref={ref}
+        radius={radius}
+        progress={progressOffset}
+        thickness={20}
+        centerText={`${secondsLeft}s`}
+        filledColor={(props) => `rgb(${props.theme.fontColorDefault})`}
+        emptyColor={(props) => `rgba(${props.theme.fontColorDefault},0.3)`}
+      />
     );
   }
 );
 
-export default styled(CountdownCircle)`
-  position: relative;
-  width: fit-content;
-  height: fit-content;
-  .circle {
-    fill: none;
-
-    &.background {
-      stroke: red;
-    }
-
-    &.progress {
-      stroke: blue;
-    }
-  }
-
-  .seconds {
-    font-size: var(--h1-size);
-    fill: ${(props) => props.theme.fontColorDefault};
-    font-weight: var(--heading-weight);
-  }
-`;
+export default styled(CountdownCircle)``;
