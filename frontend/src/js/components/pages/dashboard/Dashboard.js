576c005eed45ded4621bb0405bae90d46ef39ed5
Maja Reichert <stu126115@mail.uni-kiel.de>
2021-06-21 10:25:51 +0200

    Load daily workout on dashboard

--- frontend/src/js/components/pages/dashboard/Dashboard.js
+++ frontend/src/js/components/pages/dashboard/Dashboard.js
@@ -1,39 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import styled from 'styled-components';
 
 import { UserContext } from '../../../context';
+import { useQuery } from '../../../api/sanity';
 
 import WorkoutInfo from './WorkoutInfo';
 import { ReactComponent as WorkoutImage } from './../../../../img/svg/Programme large.svg';
 import LabelLink from './../../elements/labels/LabelLink';
+import LoadingSpinner from './../../elements/loading/LoadingSpinner';
 
 const StyledWorkoutImage = styled(WorkoutImage)`
   margin: 10px 0;
   width: 100%;
 `;
 
-const StyledDashboard = styled.div`
-  padding-top: 45px;
-  padding-bottom: var(--nav-height);
+const getDailyProgram = `*[_type == "user" && name == $userName] {
+  "title": activeProgram.ActiveProgram->.title,
+  "slug": activeProgram.ActiveProgram->.slug.current,
+  "workout": activeProgram.ActiveProgram->.workouts[
+    ^.activeProgram.day == 1 && day == 1
+    || ^.activeProgram.dateOfLastWorkoutCompletion >= $today 
+      && day == ^.activeProgram.day - 1
+    || ^.activeProgram.dateOfLastWorkoutCompletion < $today 
+      && day == ^.activeProgram.day 
+  ] { 
+    "done": day < ^.activeProgram.day,
+    day, 
+    Workout-> {
+      title,
+      duration,
+      calories,
+      categories
+    }
+  }[0]
+}[0]`;
 
-  h1 {
-    margin-bottom: 50px;
-  }
-
-  .subheader {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
-`;
-
-const Dashboard = (props) => {
+const Dashboard = ({ className }) => {
   const user = useContext(UserContext);
+  const params = useMemo(
+    () => ({
+      userName: user.name,
+      today: new Date().toISOString().split('T')[0],
+    }),
+    [user]
+  );
+  const { loading, data } = useQuery(getDailyProgram, params);
+
+  console.log(data);
   const currHours = new Date().getHours();
   const timeOfDay =
     currHours < 11 ? 'Morgen' : currHours < 17 ? 'Tag' : 'Abend';
   return (
-    <StyledDashboard>
+    <div className={className}>
       <h1>
         Guten
         <br />
@@ -47,10 +65,33 @@ const Dashboard = (props) => {
           <LabelLink to="/">Trainingsplan</LabelLink>
         </div>
         <StyledWorkoutImage />
-        <WorkoutInfo />
+        {loading ? (
+          <LoadingSpinner />
+        ) : data ? (
+          data.title ? (
+            <WorkoutInfo program={data} />
+          ) : (
+            'Kein Programm gestartet'
+          )
+        ) : (
+          'Daten konnten nicht geladen werden.'
+        )}
       </main>
-    </StyledDashboard>
+    </div>
   );
 };
 
-export default Dashboard;
+export default styled(Dashboard)`
+  padding-top: 45px;
+  padding-bottom: var(--nav-height);
+
+  h1 {
+    margin-bottom: 50px;
+  }
+
+  .subheader {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+`;
